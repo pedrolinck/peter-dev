d4e7d1c79c5a10003597a0ef89e1af4e1ef37775
pedro_linck@outlook.com <pedro_linck@outlook.com>
2024-11-24 18:49:34 -0300

    adding context to about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 
 export default function AboutPage(){
   return (
-    <h1>working on</h1>
+    <h1>
+      I started my professional career when I was 16 years old in a forniture factory on 2012 wich I worked until 2014. Then I decided to go to Brazilian Army where I had a unique experience. On 2016 I returned to my first job thinking that was my path to follow and I worked until 2018.<br />
+      On 2019 I had an unique job opportunity abroad but &apos;doesn't&apos; worked because I don't have yet a good maturity.
+      Returning to Brazil I decided to go to college on 2020 at Vale do Rio dos Sinos University where I finally changed completely my professional career. Systems Analysis and Delopment course approached a variety of concepts about the lifecycle of a software development and now I know that this is the path I want to follow.
+    </h1>
   )
 }
