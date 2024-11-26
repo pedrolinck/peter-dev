5713ca01747158c54f4c63683bc3db89bcf0f865
pedro_linck@outlook.com <pedro_linck@outlook.com>
2024-11-25 23:39:37 -0300

    adjust text view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,11 +11,13 @@ export default function HomePage(){
       <main className={main}>
         <section className="flex gap-8">
           {/* <img src="./src/assets/do-something.jpg" alt="profile picture" className="w-1/5 bg-black rounded-full" /> */}
-          <div className="">
-            <h1 className="text-6xl">You&apos;re welcome</h1>
-            <p className="text-4xl mt-16">My name is Pedro but you can call me Peter. I&apos;m a Frontend developer targeting the success.<br />
-            <span className="">Feel free!!</span>
-            </p>
+          <div className="w-full mt-16 flex justify-end">
+            <div className="w-1/2 ">
+              <h1 className="text-6xl">You&apos;re welcome</h1>
+              <p className="text-4xl mt-16">My name is Pedro but you can call me Peter. I&apos;m a Frontend developer.
+              <p className="mt-4">Feel free!!</p>
+              </p>
+            </div>
           </div>
         </section>
       </main>
