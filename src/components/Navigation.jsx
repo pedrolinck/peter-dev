31e6339e86cb91451967f367a2bb7f11bfc43e10
pedro_linck@outlook.com <pedro_linck@outlook.com>
2024-11-25 23:09:15 -0300

    add home navigation to navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,6 +7,7 @@ export default function Navigation(){
         <Link to="/">Peter_Dev<img className="inline-block ml-2" src="/favicon.png" alt="" /></Link>
       </li>
       <ul className="flex gap-4 uppercase">
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/contact">Contact</Link></li>
