7e395eee51a2094c80e09ab0dca8b53fe356b2f2
pedro_linck@outlook.com <pedro_linck@outlook.com>
2025-01-02 17:13:50 -0300

    Fix stylization and adjust profile photo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,19 @@
-
+import '../index.css';
 
 export default function HomePage(){
 
-  const main = `.main{
-    backdrop-filter: blur(5px);
-  }`
-
   return (
     <>
-      <main className={main}>
-        <section className="flex gap-8">
-          {/* <img src="./src/assets/do-something.jpg" alt="profile picture" className="w-1/5 bg-black rounded-full" /> */}
-          <div className="w-full mt-16 flex justify-end">
+      <main >
+        <section className="flex justify-evenly ">
+          <div >
+            <img className="rounded-full rounded-bl-3xl imgc" src="./src/assets/profile.png" alt="profile picture"/>
+          </div>
+          <div className="w-full flex justify-evenly">
             <div className="w-1/2 ">
-              <h1 className="text-6xl">You&apos;re welcome</h1>
-              <p className="text-4xl mt-16">My name is Pedro but you can call me Peter. I&apos;m a Frontend developer.
-              <p className="mt-4">Feel free!!</p>
+              <h1 className=" text-5xl">You&apos;re welcome!</h1>
+              <p className="text-4xl mt-16">My name is Pedro but you can call me Peter. I am a Frontend developer.
+              <p className="mt-6">Feel free!!</p>
               </p>
             </div>
           </div>
