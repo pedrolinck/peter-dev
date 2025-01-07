b0476dde0d9352fd25db6e4cff892a7603b12a27
pedro_linck@outlook.com <pedro_linck@outlook.com>
2025-01-07 08:12:26 -0300

    change about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,8 +7,7 @@ export default function AboutPage(){
         I started my professional career when I was 16 years old in a forniture factory on 2012 wich I worked until 2014. Then I decided to go to Brazilian Army where I had a unique experience. On 2016 I returned to my first job thinking that was my path to follow and I worked until 2018.
       </p>
       <p className="w-4/6 text-2xl ml-auto text-justify">
-        On 2019 I had a great job opportunity abroad but doesn&apos;t worked because I didn&apos;t have a good maturity yet.
-        Returning to Brazil, on 2020, I decided to go to college at Vale do Rio dos Sinos University where I finally changed completely my professional career. Systems Analysis and Development course approached a variety of concepts about the lifecycle of a software development and now I know that this is the path I want to follow.
+        On 2020 I decided to go to college at Vale do Rio dos Sinos University where I finally changed completely my professional career. Systems Analysis and Development course approached a variety of concepts about the software development lifecycle and showed me a lot options to figure out and explore this huge world.
       </p>
     </div>
   )
